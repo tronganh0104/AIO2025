5ac244969bd058d2b3a05348d8766365969d32cc
Bui Trong Anh <tronganhsm@gmail.com>
2026-02-16 10:57:30 +0700

    Add f1-score exercise

---
+++ Chapter 1/f1-score.py
@@ -0,0 +1,22 @@
+def f1_score(tp, fp, fn):
+    if not isinstance(tp, int):
+        print("tp must be int")
+        return
+    if not isinstance(fp, int):
+        print("fp must be int")
+        return
+    if not isinstance(fn, int):
+        print("fn must be int")
+        return
+    if tp < 0 or fp < 0 or fn < 0:
+        print("tp and fp and fn must be greater than or equal zero")
+        return
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    if precision + recall == 0:
+        print("cannot calculate f1-score (division by zero)")
+    f1_score = 2 * (precision * recall)/ (precision + recall)
+    print(f"precision is {precision}")
+    print(f"recall is {recall}")
+    print(f"f1-score is {f1_score}")
+    return precision, recall, f1_score
