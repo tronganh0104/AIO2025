7dec79549d66aec404c7b18017927059f0732fb6
Bui Trong Anh <tronganhsm@gmail.com>
2026-02-24 14:46:49 +0700

    Update chapter 4

--- Module 1/Chapter 4/Xây dựng ứng dụng tính hàm toán học giai thừa/app.py
+++ Module 1/Chapter 4/Xây dựng ứng dụng tính hàm toán học giai thừa/app.py
@@ -1,12 +1,77 @@
 import streamlit as st
 from factorial import fact
+import os
+
+def load_users():
+    try:
+        if os.path.exists("user.txt"):
+            with open("user.txt", "r", encoding="utf-8") as f:
+                users = [line.strip() for line in f.readlines() if line.strip()]
+            
+            return users
+        else:
+            st.error("File user.txt không tồn tại")
+    except Exception as e:
+        st.error(f"Lỗi khi đọc file user.txt: {e}")
+        return[]
+
+def login_page():
+    st.title("Đăng nhập")
+    
+    username = st.text_input("Nhập tên người dùng")
+
+    if st.button("Đăng nhập"):
+        if username:
+            users = load_users()
+            if username in users:
+                st.session_state.logged_in = True
+                st.session_state.username = username
+                st.rerun()
+            else:
+                st.session_state.show_greeting = True
+                st.session_state.username = username
+                st.rerun()
+        else:
+            st.warning("Vui lòng nhập tên người dùng")
+
+def factorial_calculator():
+    st.title("Factorial Calculator")
+    st.write(f"Xin chào, {st.session_state.username}")
+    if st.button("Đăng xuất"):
+        st.session_state.logged_in = False
+        st.session_state.username = ""
+        st.rerun()
+    
+    st.divider()
+
+    number = st.number_input("Vui lòng nhập một số: ", min_value=0, max_value=900)
+    if st.button("Tính giai thừa"):
+        result = fact(number)
+        st.write(f"Giai thừa của {number} là {result}")
+
+def greeting_page():
+    st.title("Xin chào")
+    st.write(f"Xin chào {st.session_state.username}")
+    st.write("Bạn không có quyền truy cập")
+    if st.button("Quay lại đăng nhập"):
+        st.session_state.show_greeting = False
+        st.session_state.username = ""
+        st.rerun()
 
 def main():
-    st.title("Factorial Calculator")
-    number = st.number_input("Enter a number: ", min_value=0, max_value=900)
-    if st.button("Calculate"):
-        result = fact(number)
-        st.write(f"The factorial of {number} is {result}")
+    if 'logged_in' not in st.session_state:
+        st.session_state.logged_in = False
+    if 'username' not in st.session_state:
+        st.session_state.username = ""
+    if 'show_greeting' not in st.session_state:
+        st.session_state.show_greeting = False
+
+    if st.session_state.logged_in:
+        factorial_calculator()
+    elif st.session_state.show_greeting:
+        greeting_page()
+    else:
+        login_page()
 
 if __name__ == "__main__":
     main()
