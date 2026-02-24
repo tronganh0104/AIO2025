21700581f7f25253469827794e828f23c3a7eecf
Bui Trong Anh <tronganhsm@gmail.com>
2026-02-24 14:09:52 +0700

    Update chapter 4

---
+++ Module 1/Chapter 4/Xây dựng ứng dụng tính hàm toán học giai thừa/app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from factorial import fact
+
+def main():
+    st.title("Factorial Calculator")
+    number = st.number_input("Enter a number: ", min_value=0, max_value=900)
+    if st.button("Calculate"):
+        result = fact(number)
+        st.write(f"The factorial of {number} is {result}")
+
+if __name__ == "__main__":
+    main()
